2811b23f9e43740723e216e12e659f63bf45a2c8
I Love Iced Coffee <107214218+CodeNinjasEtobicoke@users.noreply.github.com>
2024-05-14 18:31:22 -0400

    SO-step 59

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,28 +1,81 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    private spawner spawner;
 
+    private spawner spawner;
+    public GameObject title;
+    private Vector2 screenBounds;
+    public GameObject playerPrefab;
+    private GameObject player;
+    private bool gameStarted = false;
+    public GameObject splash;
 
     void Awake()
     {
         spawner = GameObject.Find("Spawner").GetComponent<spawner>();
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        player = playerPrefab;
     }
     // Start is called before the first frame update
     void Start()
     {
         spawner.active = false;
+        title.SetActive(true);
+        splash.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!gameStarted)
+        {
+            if (Input.anyKeyDown)
+            {
+                ResetGame();
+            }
+        }
+        else
+        {
+            if (!player)
+            {
+                OnPlayerKilled();
+            }
+        }
         if (Input.anyKeyDown)
         {
             spawner.active = true;
+            title.SetActive(false);
         }
+
+        var nextBomb = GameObject.FindGameObjectsWithTag("Bomb");
+
+        foreach (GameObject bombObject in nextBomb)
+        {
+            if(bombObject.transform.position.y < (-screenBounds.y) - 12 || !gameStarted)
+            {
+                Destroy(bombObject);
+            }
+        }
+    }
+
+
+    void OnPlayerKilled()
+    {
+        spawner.active = false;
+        gameStarted = false;
+
+        splash.SetActive(true);
+    }
+    void ResetGame()
+    {
+        spawner.active = true;
+        title.SetActive(false);
+        splash.SetActive(false);
+        player = Instantiate(playerPrefab, new Vector3(0, 0, 0), playerPrefab.transform.rotation);
+        gameStarted = true;
     }
 }
